ef04de83b159f231ec470af4403501eb8c05e681
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-22 17:47:02 -0400

    pass template to Modal through a slot making it a reusable component

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="backdrop" @click.self="closeModal">
     <div class="modal" :class="{ sale: theme == 'sale' }">
-      <h1>{{ header }}</h1>
-      <p>{{ text }}</p>
+      <slot></slot>
     </div>
   </div>
 </template>
@@ -10,7 +9,7 @@
 <script>
 export default {
   name: "Modal",
-  props: ["header", "text", "theme"],
+  props: ["theme"],
   methods: {
     closeModal() {
       this.$emit("close");
